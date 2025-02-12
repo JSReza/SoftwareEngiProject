bc5c82e8b929604fa39deedb0e93ed0a58888c5c
LoganCoop <lpcooper@gmail.com>
2025-02-12 15:14:17 -0500

    Got rid of dec to hex and added second api

---
+++ API/datastorageapi.java
@@ -0,0 +1,15 @@
+public interface DataStorageAPI {
+
+    @ProcessAPI
+    void readData();
+
+    @ProcessAPI
+    void writeData();
+
+    @ProcessAPIPrototype
+    default void processData() {
+        // Prototype method implementation
+        readData();
+        writeData();
+    }
+}
