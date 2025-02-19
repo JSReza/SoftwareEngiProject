bb1bb2146a4ef441e73b707f614f116d5d99c6f7
Jaheen Reza <rezajaheen@gmail.com>
2025-02-19 14:46:59 -0500

    small change made hors ago, dont remember specifically

--- src/DataTransferAPIImplementation.java
+++ src/DataTransferAPIImplementation.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import API_Package.*;
 public class DataTransferAPIImplementation  implements DataTransferAPI{
 
@@ -6,13 +7,16 @@ public class DataTransferAPIImplementation  implements DataTransferAPI{
     
         public DataTransferAPIImplementation(NetworkAPIImplementation networkAPI) {
             this.networkAPI = networkAPI;
-            this.bufferSize = 1024; // default buffer size
+            this.bufferSize = 1024; 
         }
     public void transferData(String source, String destination, String delimiter){
 
     }
 
     void convertDecimalToHex(int decimal, String destination){
+
+    }
+    public void sendData(List<String> list){
 
     }
 
