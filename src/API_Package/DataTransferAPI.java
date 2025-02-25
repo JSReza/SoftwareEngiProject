f779a3110534f871fe8c285f6b826aa8aa2cfe78
Jaheen Reza <rezajaheen@gmail.com>
2025-02-25 14:42:51 -0500

    Auto stash before merge of "main" and "origin/main"

--- src/API_Package/DataTransferAPI.java
+++ src/API_Package/DataTransferAPI.java
@@ -1,25 +1,7 @@
-package API_Package;
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-@NetworkAPI
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-@API_Package.NetworkAPI
-=======
-@NetworkAPI
->>>>>>> origin/main
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
-@API_Package.NetworkAPI
->>>>>>> f1be917 (fixed errors in code)
-=======
->>>>>>> Stashed changes
+
 public interface DataTransferAPI {
     void transferData(String source, String destination, String delimiter);
 
     void convertDecimalToHex(int decimal, String destination);
+}
 }
