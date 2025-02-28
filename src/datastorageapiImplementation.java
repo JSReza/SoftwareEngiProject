ecee922e17f111702b58f799dec06d463efdf8ec
LoganCoop <lpcooper@gmail.com>
2025-02-27 19:33:09 -0500

    fixed

--- src/datastorageapiImplementation.java
+++ src/datastorageapiImplementation.java
@@ -7,12 +7,6 @@ public class datastorageapiImplementation implements DataStorageAPI {
         this.storageLocation = storageLocation;
         this.isInitialized = false;
     }
-
-
-    public void readData(String st) {
-
-    }
-
     public void readData(){
 
     }
