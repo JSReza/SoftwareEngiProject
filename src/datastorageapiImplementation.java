9a1d12d91ba596c579c2a46c7e98251246253246
LoganCoop <lpcooper@gmail.com>
2025-03-05 15:01:58 -0500

    changes

--- src/datastorageapiImplementation.java
+++ src/datastorageapiImplementation.java
@@ -1,23 +1,19 @@
 import API_Package.DataStorageAPI;
-
-public class DataStorageAPIImplementation implements DataStorageAPI {
+public class datastorageapiImplementation implements DataStorageAPI {
     private final String storageLocation;
     private boolean isInitialized;
 
-    public DataStorageAPIImplementation(String storageLocation) {
+    public datastorageapiImplementation(String storageLocation) {
         this.storageLocation = storageLocation;
         this.isInitialized = false;
     }
-
-    public void readData() {
-
-    }
-
-    public void writeData() {
+    public void readData(){
 
     }
+    public void writeData(){
 
-    public void processData() {
-
+    }
+    public void processData(){
+        
     }
 }
