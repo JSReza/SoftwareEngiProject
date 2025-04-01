70db11df86047d36f2dc7dff79cb667d9bb93231
Jaheen Reza <rezajaheen@gmail.com>
2025-04-01 12:23:35 -0400

    created multithreadedNetworkAPI file in API Package folder

---
+++ API_Package/MultithreadedNetworkAPI.java
@@ -0,0 +1,10 @@
+package API_Package;
+import API_Package.NetworkAPI;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+public class MultithreadedNetworkAPI {
+   
+}
