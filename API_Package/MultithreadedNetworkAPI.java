9f78d1733f8ecc28348f911256d67421dce24ef9
Jaheen Reza <rezajaheen@gmail.com>
2025-04-02 15:35:13 -0400

    created multithreaded implementation of the networkAPI interface

--- API_Package/MultithreadedNetworkAPI.java
+++ API_Package/MultithreadedNetworkAPI.java
@@ -6,5 +6,23 @@ import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 public class MultithreadedNetworkAPI {
-   
+    private static final int MAX_THREADS = 4;
+    private final ExecutorService executorService;
+
+    public MultithreadedNetworkAPI() {
+        this.executorService = Executors.newFixedThreadPool(MAX_THREADS);
+    }
+
+    public void shutdown(){
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+           
+        }
+    }
 }
+    
