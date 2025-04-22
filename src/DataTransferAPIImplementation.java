da95ea34689ee1ffc763d75bb017abcdb5499898
Jaheen Reza <rezajaheen@gmail.com>
2025-04-22 17:11:12 -0400

    tried to fix the merge conflicts that didnt let logan merge code into code base

--- src/DataTransferAPIImplementation.java
+++ src/DataTransferAPIImplementation.java
@@ -1,40 +1,4 @@
 
 import annotations.DataTransferAPI;
-import java.util.List;
-
-public class DataTransferAPIImplementation implements DataTransferAPI {
-
-
-        final NetworkAPIImplementation networkAPI;
-        final int bufferSize;
-
-        public DataTransferAPIImplementation(NetworkAPIImplementation networkAPI){
-            this.networkAPI = networkAPI;
-            this.bufferSize = 1024;
-        }
-
-
-
-    public void transferData(String source, String destination, String delimiter) throws IllegalStateException{
-        try{
-            //transfer data
-        }catch(IllegalStateException e){
-            //
-        }
-    }
-
-    public void convertDecimalToHex(int decimal, String destination) {
-
-    }
-
-    @Override
-    public void sendData() {
-
-    }
-
-    public void sendData(List<String> outputData) {
-    }
-}
-
 
 
